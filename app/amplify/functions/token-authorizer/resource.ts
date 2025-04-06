46c9a99d438658c399fcfa7ec5a95ce09d1d7184
Gavin Palmer <gavin@herolfg.com>
2025-04-06 12:33:24 -0500

    fix(lambda): Assign tokenAuthorizer to data resource group to fix circular dependency

--- app/amplify/functions/token-authorizer/resource.ts
+++ app/amplify/functions/token-authorizer/resource.ts
@@ -2,6 +2,8 @@ import { defineFunction } from '@aws-amplify/backend';
 
 export const tokenAuthorizerFunction = defineFunction({
   name: 'pagehub-token-authorizer',
+  // Assign this function to the 'data' resource group to resolve circular dependency
+  resourceGroupName: 'data',
   // entry: './handler.ts' // Handler file next
   // Needs env vars for AppSync API ID / Region
   // Needs IAM permissions for appsync:GraphQL (to call getAccessToken query)
